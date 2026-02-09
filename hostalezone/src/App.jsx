6bc69570195725af13b0b38e60d89235f0333109
Ometh Widmal <withmalwijesiri@gmail.com>
2026-02-09 11:43:03 +0530

    Add files via upload

---
+++ hostalezone/src/App.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+
+// Hostel website components
+import NaveBar from "./components/NaveBar/NaveBar";
+import Footer from "./components/Footer/Footer";
+import HeroSection from "./components/Home/HeroSection";
+import About from "./components/Home/About";
+import Contact from "./components/Home/Contact";
+import Rules from "./components/Home/Rules";
+import Rooms from "./components/Home/Rooms";
+import Dmodel from "./components/Home/Dmodel";
+
+
+
+// Home Page Component
+function Home() {
+  return (
+    <>
+      <NaveBar />
+      <HeroSection />
+      <Rooms />
+      
+      <About />
+      <Dmodel /> {/* Your 3D model */}
+      <Contact />
+      <Rules />
+      <Footer />
+    </>
+  );
+}
+
+// Another page/component example for the "One" component
+function ExternalView() {
+  return (
+    <div className="App">
+      <Dmodel 
+        websiteUrl="https://your-website.com"
+        websiteTitle="Your Website"
+        showControls={true}
+        autoRotate={false}
+      />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/external" element={<ExternalView />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
